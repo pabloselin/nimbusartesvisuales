10aec1260b3f8e92bd15c7fbbdb07817048b5a8d
Pablo Selin <pabloselin@gmail.com>
2022-01-31 16:18:57 +0100

    loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,24 @@
 import Box from "@mui/material/Box";
 import { useTheme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
+import { styled } from "@mui/system";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const StyledTypography = styled(Typography)`
+	.icon {
+		animation: pulse 1s infinite;
+		animation-delay: calc(var(--index) * -0.25s);
+
+		@keyframes pulse {
+			0% {
+				opacity: 0.2;
+			}
+			100% {
+				opacity: 1;
+			}
+		}
+	}
+`;
 
 const Loading = (props) => {
 	const theme = useTheme();
@@ -12,13 +29,17 @@ const Loading = (props) => {
 				padding: "24px",
 				textAlign: "center",
 				backgroundColor: theme.palette.background.default,
+				color: theme.palette.primary.main,
 			}}
 		>
-			<Typography variant="body1">
-				<FontAwesomeIcon size="2x" icon="wind" />
-				<br />
+			<StyledTypography variant="body1" sx={{ textAlign: "center" }}>
+				<p>
+					<FontAwesomeIcon size="2x" icon="wind" className="icon" />
+					<FontAwesomeIcon size="2x" icon="wind" className="icon" />
+					<FontAwesomeIcon size="2x" icon="wind" className="icon" />
+				</p>
 				Cargando ...
-			</Typography>
+			</StyledTypography>
 		</Box>
 	);
 };
